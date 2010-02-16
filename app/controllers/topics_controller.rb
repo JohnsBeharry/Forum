707e5d6a85e688dc7a8fe2513f67bef9933f56af
Johns Beharry <info@johnsbeharry.com>
2010-02-15 23:31:39 -0400

    Added topic_controller from the tut

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,44 +1,15 @@
-class TopicsController < ApplicationController
-  def index
-    @topics = Topic.all
-  end
-  
-  def show
-    @topic = Topic.find(params[:id])
-  end
-  
-  def new
-    @topic = Topic.new
-  end
-  
-  def create
-    @topic = Topic.new(params[:topic])
-    if @topic.save
+def create
+  @topic = Topic.new(params[:topic])
+  if @topic.save
+    @topic = Topic.new(:name => params[:topic][:name], :last_poster_id => current_user.id, :last_post_at => Time.now, :forum_id => params[:topic][:forum_id])
+
+    if @post.save
       flash[:notice] = "Successfully created topic."
-      redirect_to @topic
+      redirect_to "/forums/#{@topic.forum_id}"
     else
-      render :action => 'new'
+      redirect :action => 'new'
     end
-  end
-  
-  def edit
-    @topic = Topic.find(params[:id])
-  end
-  
-  def update
-    @topic = Topic.find(params[:id])
-    if @topic.update_attributes(params[:topic])
-      flash[:notice] = "Successfully updated topic."
-      redirect_to @topic
-    else
-      render :action => 'edit'
-    end
-  end
-  
-  def destroy
-    @topic = Topic.find(params[:id])
-    @topic.destroy
-    flash[:notice] = "Successfully destroyed topic."
-    redirect_to topics_url
+  else
+    render :action => 'new'
   end
 end
